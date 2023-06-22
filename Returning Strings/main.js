09df4717b42372dbca36b8503471bcf163204a27
elena-brd <128292991+elena-brd@users.noreply.github.com>
2023-06-22 16:26:49 +0200

    Add files via upload

---
+++ Returning Strings/main.js
@@ -0,0 +1,8 @@
+function greet(name) {
+    return `Hello, ${name} how are you doing today? `;
+}
+
+const greet2 = (name) => `Hello, ${name} how are you doing today? `;
+
+console.log(greet('Jane'));
+console.log(greet2('Joe'));
